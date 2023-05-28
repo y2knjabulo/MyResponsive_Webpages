e7fe25b31eeb61d35c41001990182ffe0dbd5a70
Njabulo <y2knjabulo00@gmail.com>
2023-05-28 14:00:27 +0200

    second commit

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,22 @@
-import data from './data.json';
 import handlebars from 'vite-plugin-handlebars';
+import { defineConfig } from 'vite';
+import data from './data.json';
 
-export default {
+export default defineConfig({
+    base : '/MyResponsive_Webpages/',
+    build: {
+        rollupOptions: {
+            input : {
+                main: 'index.html',
+                bootstrap: 'index-bootstrap.html',
+                materialize: 'index-materialize.html',
+            }
+        },
+    },
     plugins: [handlebars({
-      context : data 
-
-    })],
-  };
+      context : {
+        data
+      }
+  })]
+    
+});
