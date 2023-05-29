db100fb8ed8ce1b326eedbee6886db621d325e4e
Njabulo <y2knjabulo00@gmail.com>
2023-05-29 10:20:19 +0200

    new chnages

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,22 @@
-
 import handlebars from 'vite-plugin-handlebars';
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
+import data from './data.json';
 
 export default defineConfig({
-   build: {
-     rollupOptions: {
-       input : {
-         main: 'index.html',
-         one: 'one.html',
-       }
-     },
-   },
-   base: '/vite-hbs/',
-   plugins: [handlebars({
-       context : {
-         title : "Sibu & Thabang",
-         fruits : ["apple", "banana", "orange", "pear", "litchi"]
-       }
-   })]
-})
+    base : '/responsive_webpages/',
+    build: {
+        rollupOptions: {
+            input : {
+                main: 'index.html',
+                bootstrap: 'index-bootstrap.html',
+                materialize: 'index-materialize.html',
+            }
+        },
+    },
+    plugins: [handlebars({
+      context : {
+        data
+      }
+  })]
+    
+});
