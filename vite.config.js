a41d3a715c5b4ef6ad17ca81f4c501575bc70fce
Njabulo <y2knjabulo00@gmail.com>
2023-05-29 10:27:21 +0200

    neede files

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
         rollupOptions: {
             input : {
                 main: 'index.html',
-                bootstrap: 'index-bootstrap.html',
+                bootstrap: 'index-boostrap.html',
                 materialize: 'index-materialize.html',
             }
         },
